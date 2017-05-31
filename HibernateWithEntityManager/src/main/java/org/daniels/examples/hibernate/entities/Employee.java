66ab209d9ce4476edb7fee30e7318b259705f36e
Daniels <daniel.sadowski75@gmail.com>
2017-05-31 23:09:54 +0200

    init commit

---
+++ HibernateWithEntityManager/src/main/java/org/daniels/examples/hibernate/entities/Employee.java
@@ -0,0 +1,87 @@
+package org.daniels.examples.hibernate.entities;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+@Entity
+@Table(name="EMPLOYEE")
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "employee_id")
+    private Long employeeId;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @OneToMany(mappedBy="employee")
+    private List<Address> addresses = Lists.newArrayList();
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public void add(Address address) {
+        this.addresses.add(address);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", addresses=" + addresses +
+                '}';
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+#HibernateWithEntityManager - an example with 3 entities and EntityManger - uses a MySQL database
